4d7d8c3fa3cf14fa02ebfaae0692f8dfb461c190
Maricus Samson <macadadi36@gmail.com>
2022-06-18 22:15:30 +0300

    update token

--- src/Api/Authenticate.js
+++ src/Api/Authenticate.js
@@ -1,34 +1,21 @@
 import React, { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import { GoogleLogin } from "@react-oauth/google";
 
 function Authenticate() {
   const handleCookies = (response) => {
     localStorage.setItem("dtone_id", response.credential);
     window.location.reload();
   };
-  const client__id =
-    "300013803126-j7e8khlvmjftmnkjfn5rtvdtf4hp2eoc.apps.googleusercontent.com";
 
-  useEffect(() => {
-    try {
-      /*global google*/
-      if (google !== undefined) {
-        google.accounts.id.initialize({
-          client_id: client__id,
-          callback: handleCookies,
-        });
-        google.accounts.id.renderButton(document.getElementById("theme"), {
-          theme: "outline",
-          size: "large",
-        });
-      }
-    } catch (err) {
-      throw err;
-    }
-  }, []); 
   return (
     <div>
-      <button id="theme"></button>
+      <GoogleLogin
+        onSuccess={handleCookies}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
     </div>
   );
 }
@@ -51,8 +38,6 @@ export const IsAuthentcated = () => {
       setUser();
     }
   }, [user]);
-
-
 
   return { user, setUser, logUserOut };
 };
